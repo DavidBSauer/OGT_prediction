8c640f33fe6b6972801a358a787937d9e5fe0e3d
david <david.sauer@med.nyu.edu>
2018-10-22 21:43:29 -0400

    correctly record prodigal version

--- feature_calculation/external_tools.py
+++ feature_calculation/external_tools.py
@@ -31,7 +31,7 @@ def versions():
 	p = subprocess.Popen([commands['barrnap']+' --version'],shell=True,executable='/bin/bash',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out,err = p.communicate()
 	logger.info('barrnap version info: '+err.decode('utf-8').strip())
-	p = subprocess.Popen([commands['prodigal']+' --v'],shell=True,executable='/bin/bash',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	p = subprocess.Popen([commands['prodigal']+' -v'],shell=True,executable='/bin/bash',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out,err = p.communicate()
 	logger.info('prodigal version info: '+out.decode('utf-8').strip())
 	logger.info('Numpy version: '+np.__version__)
