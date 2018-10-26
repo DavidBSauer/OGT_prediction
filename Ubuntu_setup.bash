6b81b37589a70eb507c7d17aee857ce2d3eac7f6
David B Sauer <40378905+DavidBSauer@users.noreply.github.com>
2018-10-26 15:54:47 -0400

    proper location

--- Ubuntu_setup.bash
+++ Ubuntu_setup.bash
@@ -58,7 +58,7 @@ echo "install python packages"
 sudo pip3 install numpy scipy matplotlib biopython bcbio-gff tqdm sklearn matplotlib matplotlib-venn
 
 #move external_tools.txt
-cp external_tools.txt ./feature_calculation/external_tools.txt
-cp external_tools.txt ./prediction/external_tools.txt
+cp ../external_tools.txt ../feature_calculation/external_tools.txt
+cp ../external_tools.txt ../prediction/external_tools.txt
 
 echo "finished"
